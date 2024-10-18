b19911bb7f76c46e7e5faf542cecf6a6fa276704
AmziJeffs <amzijeffs0@gmail.com>
2024-10-18 17:52:42 -0400

    Add ability to update solver docstrings, to insert grids for finetuning

--- DSL/solver_class.py
+++ DSL/solver_class.py
@@ -21,7 +21,8 @@ class Solver():
         """
         self.name = program_text.split("(I)")[0].split(" ")[1]
         self.program_text = program_text
-        self.update_function_from_text()
+        self.function = self.update_function_from_text()
+        self.docstring = self.update_docstring_from_text()
 
     def update_function_from_text(self):
         """
@@ -29,6 +30,32 @@ class Solver():
         """
         exec(self.program_text)
         self.function = eval(self.name)
+        return self.function
+
+    def update_docstring_from_text(self):
+        """
+        Read program_text to get docstring
+        """
+        if '"""\n' not in self.program_text:
+            self.docstring = ""
+        else:
+            self.docstring = self.program_text.split('"""\n')[1]
+        return self.docstring
+
+    def update_docstring(self, new_docstring):
+        """
+        new_docstring should not include ''' or similar. It will be inserted between
+        triple quotes in program_text. 
+        """
+        self.docstring = new_docstring
+        if '"""\n' not in self.program_text:
+            lines = self.program_text.split("\n")
+            lines = [lines[0], '"""', self.docstring, '"""'] + lines[1:]
+            self.program_text = "\n".join(lines)
+        else:
+            blocks = self.program_text.split('"""\n')
+            blocks = [blocks[0], self.docstring + "\n"] + blocks[2:]
+            self.program_text = '"""\n'.join(blocks)
 
     def __call__(self, input_grid):
         return self.function(input_grid)
