dea45be51e825067d20c19fe0f43f3ead8efaea8
AmziJeffs <amzijeffs0@gmail.com>
2024-11-06 13:45:51 -0500

    Fix variable naming solve_4093f84a

--- DSL/solvers.py
+++ DSL/solvers.py
@@ -3249,8 +3249,8 @@ def solve_4093f84a(I):
     x2 = replace(I, x1, FIVE)
     x3 = ofcolor(I, FIVE)
     x4 = portrait(x3)
-    m5 = branch(x4, identity, dmirror)
-    x6 = m5(x2)
+    x5 = branch(x4, identity, dmirror)
+    x6 = x5(x2)
     x7 = lefthalf(x6)
     x8 = righthalf(x6)
     x9 = rbind(order, identity)
@@ -3258,7 +3258,7 @@ def solve_4093f84a(I):
     x11 = apply(x9, x7)
     x12 = apply(x10, x8)
     x13 = hconcat(x11, x12)
-    O = m5(x13)
+    O = x5(x13)
     return O
 
 
